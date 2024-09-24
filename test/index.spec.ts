b2575b1a4ffde936dd32eb57d94b43573e67589d
Alexander Harbavets <alexander.gorbovets@gmail.com>
2024-09-24 19:52:19 +0200

    Fixed installation issues

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,6 @@
 import {describe, it} from 'mocha'
 import {assert} from 'chai'
-import {SimpleDate, WeekStartDay} from '../SimpleDate'
+import {SimpleDate, WeekStartDay} from '../index'
 
 describe('index', () => {
 
